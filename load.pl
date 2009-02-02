75f810c77e7e260817e5e16643646e3e1c845dd2
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-02-02 15:10:09 +0100

    Added show_fd/0

--- load.pl
+++ load.pl
@@ -1,3 +1,15 @@
 :- load_files(plweb,
 	      [ silent(true)
 	      ]).
+
+%%	show_fd
+%
+%	Show open file descriptors.  Sanity-check   that  works  only on
+%	Linux systems.
+
+show_fd :-
+        current_prolog_flag(pid, Pid),
+        format(string(Cmd),
+               '/bin/sh -c "(cd /proc/~w/fd && ls -l | grep socket)"',
+               [Pid]),
+        shell(Cmd).
