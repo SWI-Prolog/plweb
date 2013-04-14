be29d6e06c318a536a75090516bd0a0576addfb9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-04-14 11:49:13 +0200

    First steps adding feedback widgets

---
+++ markitup.pl
@@ -0,0 +1,83 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        J.Wielemaker@cs.vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2013, VU University Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+:- module(markitup,
+	  [ markitup//1
+	  ]).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/html_head)).
+:- use_module(library(http/http_parameters)).
+:- use_module(library(http/html_write)).
+:- use_module(markdown).
+
+:- http_handler(root('markitup/preview/markdown'), preview_markdown, []).
+
+:- html_resource(js('markitup/jquery.markitup.js'),
+		 [ requires([ jquery
+			    ])
+		 ]).
+:- html_resource(js('markitup/sets/markdown/set.js'),
+		 [ requires([ js('markitup/jquery.markitup.js'),
+			      js('markitup/skins/markitup/style.css'),
+			      js('markitup/sets/markdown/style.css')
+			    ])
+		 ]).
+
+markitup(Options) -->
+	{ option(id(Id), Options, pldoc),
+	  option(cols(Cols), Options, 80),
+	  option(rows(Rows), Options, 20),
+	  option(value(Content), Options, [])
+	},
+	html_requires(js('markitup/sets/markdown/set.js')),
+	html([ textarea([id(Id), cols(Cols), rows(Rows)], Content),
+	       script(type('text/javascript'),
+		      \[ '$(document).ready(function(){\n',
+			 '$("#',Id,'").markItUp(mySettings);\n',
+			 '});\n'
+		       ])
+	     ]).
+
+
+preview_markdown(Request) :-
+	http_parameters(Request,
+			[ data(Data, [])
+			]),
+	debug(markitup(preview), 'Preview:~n~w~n', [Data]),
+	open_atom_stream(Data, In),
+	markdown_dom(stream(In), DOM),
+	phrase(html(DOM), Tokens),
+	format('Content-type: text/html; charset=UTF-8\n\n'),
+	print_html(Tokens).
+
+open_atom_stream(Atom, Stream) :-
+	atom_to_memory_file(Atom, MF),
+	open_memory_file(MF, read, Stream,
+			 [ free_on_close(true)
+			 ]).
---
+++ rating.pl
@@ -0,0 +1,85 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        J.Wielemaker@cs.vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2013, VU University Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+:- module(rating,
+	  [ rate//1
+	  ]).
+:- use_module(library(http/http_path)).
+:- use_module(library(http/http_dispatch), []).
+:- use_module(library(http/html_head)).
+:- use_module(library(http/html_write)).
+
+:- multifile http:location/3.
+:- dynamic   http:location/3.
+
+http:location(jq, root('js/jquery'), []).
+
+:- html_resource(jquery,
+		 [ virtual(true),
+		   requires([ jq('jquery.js')
+			    ])
+		 ]).
+:- html_resource(jq('jRating.jquery.min.js'),
+		 [ requires([ jquery,
+			      jq('jRating.jquery.css')
+			    ])
+		 ]).
+
+
+/** <module> Deal with user feedback
+*/
+
+rate(Options) -->
+	{ option(data_id(Id), Options, rating),
+	  option(on_rating(OnRating), Options, '/on_rating'),
+	  option(length(Length), Options, 5),
+	  option(rate_max(RateMax), Options, 20),
+	  option(step(Step), Options, false),
+	  option(type(Type), Options, big),
+	  http_absolute_location(jq('icons/stars.png'), BSP, []),
+	  http_absolute_location(jq('icons/small.png'), SSP, [])
+	},
+	html_requires(jq('jRating.jquery.min.js')),
+	html([ div([ class(jrating), 'data-id'(Id)], []),
+	       script(type('text/javascript'),
+		      \[ '$(document).ready(function(){\n',
+			 '$(".jrating").jRating(\n',
+			 '   { bigStarsPath:"',BSP,'",\n',
+			 '     smallStarsPath:"',SSP,'",\n',
+			 '     phpPath:"',OnRating,'",\n',
+			 '     step:',Step,',\n',
+			 '     type:"',Type,'",\n',
+			 '     length:',Length,',\n',
+			 '     rateMax:',RateMax,',\n',
+			 '   });\n',
+			 '});\n'
+		       ])
+
+	     ]).
+
