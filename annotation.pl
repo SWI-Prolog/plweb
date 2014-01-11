51afdb86383f0c7eb3626803f8081bd102af067a
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-01-11 17:43:18 +0100

    Updated layout and local predicate names.  Added basic comments.

--- annotation.pl
+++ annotation.pl
@@ -1,12 +1,8 @@
-:- module(
-  annotation,
-  [
-    annotation//1, % +Object:compound
-    user_annotations//1, % +User:atom
-    user_annotation_count/2	% +User:atom
-                            % -Count:nonneg
-  ]
-).
+:- module(annotation,
+	  [ annotation//1,			% +object:compound
+	    user_annotations//1,		% +User:atom
+	    user_annotation_count/2		% +User:atom, -Count:nonneg
+	  ]).
 
 /** <module> Annotation
 
@@ -35,100 +31,121 @@
 http:location(annotation, root(annotation), []).
 :- http_handler(root(annotation), annotation_process, [prefix]).
 
+%%	annotation_process(+Request)
+%
+%	REST HTTP handler for /annotation/ID
+%
+%	@tbd	Where is this used for?  This also seems to do a request
+%		on ourselves.  We'd like to avoid that.
 
 annotation_process(Request):-
-  memberchk(method(get), Request),
-  request_to_resource(Request, annotation, URL), !,
-  http_get(URL, JSON, []),
-  json_to_prolog(JSON, post:Post),
-  post(Post, id, Id),
-  post(Post, about, About),
-  object_id(Object, About),
-  object_label(Object, Label),
-  atomic_list_concat(['Annotation',Label], '--', Title),
-  reply_html_page(wiki(Title), title(Title), \post([], About, Id)).
+	memberchk(method(get), Request),
+	request_to_resource(Request, annotation, URL), !,
+	http_get(URL, JSON, []),
+	json_to_prolog(JSON, post:Post),
+	post(Post, id, Id),
+	post(Post, about, About),
+	object_id(Object, About),
+	object_label(Object, Label),
+	atomic_list_concat(['Annotation',Label], '--', Title),
+	reply_html_page(wiki(Title), title(Title), \post([], About, Id)).
 annotation_process(Request):-
-  post_process(Request, annotation).
+	post_process(Request, annotation).
+
+%%	annotation(+Object)//
+%
+%	Show annotations for Object.
 
 annotation(Object1) -->
-  {
-    ground(Object1),
-    (
-      prolog:doc_canonical_object(Object1, Object2)
-    ->
-      true
-    ;
-      Object2 = Object1
-    ),
-    object_id(Object2, About), !,
-    find_posts(annotation, '_check1'(About), Ids)
-  },
-  html([
-    \html_requires(css('annotation.css')),
-    \posts(annotation, About, Ids)
-  ]).
-'_check1'(About, Id):- post(Id, about, About).
+	{ ground(Object1),
+	  (   prolog:doc_canonical_object(Object1, Object2)
+	  ->  true
+	  ;   Object2 = Object1
+	  ),
+	  object_id(Object2, About), !,
+	  find_posts(annotation, about_post(About), Ids)
+	},
+	html([\html_requires(css('annotation.css')),
+	      \posts(annotation, About, Ids)
+	     ]).
+
+about_post(About, Id) :-
+	post(Id, about, About).
+
+%%	user_annotations(+User)//
+%
+%	Show annotations created by a specific user.
 
 user_annotations(User) -->
-  {
-    find_posts(annotation, '_check2'(User), Ids),
-    Ids \== [], !,
-    sort_posts(Ids, SortedIds),
-    site_user_property(User, name(Name))
-  },
-  html([
-    \html_requires(css('annotation.css')),
-    h2(class(wiki), 'Comments by ~w'-[Name]),
-    table(class('user-comments'), \list_annotated_objects(SortedIds))
-  ]).
-user_annotations(_) --> [].
-'_check2'(User, Id):- post(Id, author, User).
+	{ find_posts(annotation, user_post(User), Ids),
+	  Ids \== [], !,
+	  sort_posts(Ids, SortedIds),
+	  site_user_property(User, name(Name))
+	},
+	html([ \html_requires(css('annotation.css')),
+	       h2(class(wiki), 'Comments by ~w'-[Name]),
+	       table(class('user-comments'),
+		     \list_annotated_objects(SortedIds))
+	     ]).
+user_annotations(_) -->
+	[].
+
+user_post(User, Id) :-
+	post(Id, author, User).
 
 list_annotated_objects([]) --> [].
 list_annotated_objects([H|T]) -->
-  {
-    post(H, about, ObjectId),
-    object_id(Object1, ObjectId),
-    Object1 = comment(Object2,Comment)
-  },
-  html([
-    tr([
-      td(\object_ref(Object2, [])),
-      td(class('comment-summary'), \comment_summary(Comment))
-    ]),
-    \list_annotated_objects(T)
-  ]).
+	{ post(H, about, ObjectId),
+	  object_id(Object1, ObjectId),
+	  Object1 = comment(Object2,Comment)
+	},
+	html([ tr([ td(\object_ref(Object2, [])),
+		    td(class('comment-summary'),
+		       \comment_summary(Comment))
+		  ]),
+	       \list_annotated_objects(T)
+	     ]).
+
+%%	comment_summary(+Comment)//
+%
+%	Show the first sentence or max first 80 characters of Comment.
 
 comment_summary(Comment) -->
-  {summary_sentence(Comment, Summary)},
-  html(Summary).
+	{ summary_sentence(Comment, Summary) },
+	html(Summary).
 
 summary_sentence(Comment, Summary):-
-  atom_codes(Comment, Codes),
-  phrase(summary(SummaryCodes, 80), Codes, _),
-  atom_codes(Summary, SummaryCodes).
+	atom_codes(Comment, Codes),
+	phrase(summary(SummaryCodes, 80), Codes, _),
+	atom_codes(Summary, SummaryCodes).
 
 summary([C,End], _) -->
-  [C,End],
-  {
-    \+ code_type(C, period),
-    code_type(End, period) % ., !, ?
-  },
-  white, !.
+	[C,End],
+	{ \+ code_type(C, period),
+	  code_type(End, period) % ., !, ?
+	},
+	white, !.
 summary([0' |T0], Max) -->
-  blank, !,
-  blanks,
-  {Left is Max-1},
-  summary(T0, Left).
+	blank, !,
+	blanks,
+	{Left is Max-1},
+	summary(T0, Left).
 summary(Elipsis, 0) --> !,
-  {string_codes(" ...", Elipsis)}.
+	{ string_codes(" ...", Elipsis)
+	}.
 summary([H|T0], Max) -->
-  [H], !,
-  {Left is Max-1},
-  summary(T0, Left).
-summary([], _) --> [].
+	[H], !,
+	{Left is Max-1},
+	summary(T0, Left).
+summary([], _) -->
+	[].
+
+%%	user_annotation_count(+User, -Count) is det.
+%
+%	True when Count is the number of object annotations created by
+%	User.
 
 user_annotation_count(User, Count) :-
-  find_posts(annotation, '_check2'(User), Annotations),
-  length(Annotations, Count).
+	find_posts(annotation, user_post(User), Annotations),
+	length(Annotations, Count).
 
