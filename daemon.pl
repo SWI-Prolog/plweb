5a677c77863bfc23de6654fd47f515bfbf27c0cb
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-08-16 21:22:32 +0200

    Schedule logfile rotation

--- daemon.pl
+++ daemon.pl
@@ -1,6 +1,12 @@
-% Avoid that XPCE creates a thread because that stops us forking.
+:- use_module(library(http/http_log)).
 
+% Avoid that XPCE creates a thread because that stops us forking.
 :- set_prolog_flag(xpce_threaded, false).
 :- load_files([load], [silent(true)]).
+
+:- http_schedule_logrotate(monthly(1, 04:00),
+			   [ keep_logs(6)
+			   ]).
+
 :- initialization http_daemon.
 
