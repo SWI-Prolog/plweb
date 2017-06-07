515e21c96937860bab945bce1e953c0fd6365113
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-06-07 11:38:58 +0200

    Use new initialization/2 directive

--- daemon.pl
+++ daemon.pl
@@ -2,11 +2,11 @@
 
 % Avoid that XPCE creates a thread because that stops us forking.
 :- set_prolog_flag(xpce_threaded, false).
-:- load_files([load], [silent(true)]).
+:- [load].
 
 :- http_schedule_logrotate(monthly(1, 04:00),
 			   [ keep_logs(6)
 			   ]).
-
-:- initialization http_daemon.
+% library(http/http_unix_daemon) is loaded when loading the library
+:- initialization(http_daemon, main).
 
